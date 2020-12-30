b80965c70f265a395bc0ef4a329787de4189d0ff
brennandoehring <nannerbdale@gmail.com>
2020-12-29 20:56:38 -0500

    Full functionality of user search/api functioning

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles.css";
+
+function Jumbotron() {
+  return (
+    <div>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">User Directory</h1>
+        <p class="lead">
+          List of users.
+        </p>
+      </div>
+    </div>
+    </div>
+  );
+}
+export default Jumbotron;
