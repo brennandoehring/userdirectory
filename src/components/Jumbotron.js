e4f19ca296802ad6d5c4ad5fff9065794142d6a1
brennandoehring <nannerbdale@gmail.com>
2020-12-30 18:07:14 -0500

    Added title name

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "./styles.css";
+import { Helmet } from 'react-helmet';
+
+const TITLE = "User Directory";
 
 function Jumbotron() {
   return (
     <div>
+      <Helmet>
+        <title> {TITLE} </title>
+      </Helmet>
     <div class="jumbotron jumbotron-fluid">
       <div class="container">
         <h1 class="display-4">User Directory</h1>
         <p class="lead">
-          List of users.
+          This is a list of your employees.
         </p>
       </div>
     </div>
